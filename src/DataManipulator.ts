480212f3f8dc858fa6457d76476f246099831e6a
HJM-Depaul <bjhj0230@gmail.com>
2023-03-06 15:16:41 -0500

    1. Added tracker the ratio between two stocks over time 2. Added monitor bounds with upper and lower bounds. And it shows trigger alerts when it passes each bound.

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,43 @@
+//Process the raw stock data we receive from the server before the Graph component renders it.
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  //Matched with Graph.tsx's perspectives.
+  //Determines the structure of the object returned by the generateRow function
+  price_abc: number,
+  price_def: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  //Process raw server data
+  static generateRow(serverRespond: ServerRespond[]): Row {
+    //Access serverRespond as an an array wherein [0] is stock ABC and [1] is stock DEF.
+    const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) /2;
+    const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) /2;
+    const ratio = priceABC / priceDEF;
+    //Be able to maintain datas as steady upper and lower lines on the graph.
+    const upperBound = 1 + 0.02;
+    const lowerBound = 1 - 0.02;
+    return {
+      //Changed to a single Row Object.
+      //It explains why we also adjusted the argument we passed to table.update in Graph.tsx so that consistency is preserved.
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ?
+        serverRespond[0].timestamp : serverRespond[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      //Has a value if the threshold is passed by the ratio
+      //Otherwise if the ratio remains within the threshold, no value/undefined will suffice.
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
+    };
+ 
   }
 }
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import { Table, TableData } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
@@ -22,11 +22,20 @@ class Graph extends Component<IProps, {}> {
     // Get element from the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
+    // Configure the perspective table on schema.
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+      //To calculate the ratio, we need price_abc and price_def.
+      price_abc: 'float',
+      price_def: 'float',
+      //Get two stocks ratio.
+      ratio: 'float',
+      //Track with respect to time.
       timestamp: 'date',
+      //Track upper_bound, lower_bound.
+      upper_bound: 'float',
+      lower_bound: 'float',
+      //Track the moment when upper_bound and lower_bound are crossed.
+      trigger_alert: 'float',
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -35,24 +44,31 @@ class Graph extends Component<IProps, {}> {
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
+      //'view'- visualize the data.
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
+      //'row-pivots' takes care of x-axis and allows us to map each datapoint based on its timestamp.
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      //'columns' allows to focus on a particular part of a datapoint's data along the y-axis.
+      elem.setAttribute('columns', '["ratio","lower_bound", "upper_bound", "trigger_alert"]');
+      //'aggregates' allows us to handle the duplicate data and consolidate them into one data point.
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
         timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
       }));
     }
   }
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
+      //Get executed whenver the component dupdates, i.e. when the graph gets new data.
+      this.table.update([
         DataManipulator.generateRow(this.props.data),
-      );
+      ] as unknown as TableData);
     }
   }
 }
